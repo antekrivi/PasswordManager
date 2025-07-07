e5a02ed2cc51488dc12128a3bcf14002f5adc966
antekrivi <ante.krivacic@gmail.com>
2025-07-07 22:19:02 +0200

    Ignore .env file

--- backend/src/config/database.config.ts
+++ backend/src/config/database.config.ts
@@ -1,10 +1,8 @@
 import { connect, ConnectOptions } from "mongoose"
 
-const MONGO_URI = "mongodb+srv://antekrivacic:H528TD-HaC.9rUi@zavrsni.5dvmgfs.mongodb.net/?retryWrites=true&w=majority&appName=zavrsni";
-
 
 export const dbConnect = () => {
-    connect(MONGO_URI, {} as ConnectOptions).then(
+    connect(process.env.MONGO_URI!, {} as ConnectOptions).then(
         () => console.log("Connected successfully!"),
         (erorr) => console.log(erorr)
     )
